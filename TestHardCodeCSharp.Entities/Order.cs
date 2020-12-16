fe40097a5dd7bd9c66b7a194f8fc846b66a3803c
yinxingyuan <545192270@qq.com>
2020-12-16 12:06:20 +0800

    Update Order.cs

--- TestHardCodeCSharp.Entities/Order.cs
+++ TestHardCodeCSharp.Entities/Order.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Runtime.Serialization;
 using MetaShare.Common.Core.Entities;
 /*add customized code between this region*/
+using Sysem.Web;
 /*add customized code between this region*/
 
 namespace TestHardCodeCSharp.Entities
@@ -10,6 +11,7 @@ namespace TestHardCodeCSharp.Entities
 	public class Order : MetaShare.Common.Core.Entities.Common
 	{
 	/*add customized code between this region*/
+	public string Code;
 	/*add customized code between this region*/
 	}
 }
